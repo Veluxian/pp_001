f14e9958c89e2d4e1e8a8cfd0355c630352f26f4
Joaquin Orellana <joaquinosotelo@gmail.com>
2025-02-08 13:18:17 -0300

    arreglamos los controladores quitando los innecesarios, y realizamos la prueba al traer todos los tipos de monedas, ademas arreglamos el error del cointype

---
+++ back_pp_001/Controllers/currencyController.cs
@@ -0,0 +1,33 @@
+﻿using back_pp_001.Data;
+using back_pp_001.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace back_pp_001.Controllers
+{
+    [ApiController]
+    [Route("api/currency")]
+    public class currencyController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public currencyController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<CoinType>>> GetCoins()
+        {
+            var coins = await _context.CoinTypes.ToListAsync();
+
+            if (coins == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(coins);
+        }
+    }
+}
