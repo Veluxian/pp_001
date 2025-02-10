23f10c991237472ae8b9bb06f79455ddfe7443f8
joaquin Mesa <joaquinosotelo@gmail.com>
2025-02-10 17:56:43 -0300

    instalamos sass y damos el primer centrar del div

--- front_pp_01/src/components/navbar/Navbar.js
+++ front_pp_01/src/components/navbar/Navbar.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 export const Navbar = () => {
     return(
-        <nav>
+        <nav className="container">
             <ul>
                 <li><Link to= "/">Home</Link></li>
                 <li><Link to ="/login">Login</Link></li>
--- front_pp_01/src/pages/home/Home.js
+++ front_pp_01/src/pages/home/Home.js
@@ -1,8 +1,8 @@
 import React from "react";
-
+import "./home.scss";
 export const Home = () => {
     return(
-        <div>
+        <div className="container">
             <h1>Primera prueba</h1>
             <p> Probando diferentes cosas</p>
         </div>
