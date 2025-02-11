d22c0093f1ad769cd5af41f3aca4d524402af568
Joaquin Orellana <joaquinosotelo@gmail.com>
2025-02-10 22:31:20 -0300

    Permitimos que se conectaran a la api

--- back_pp_001/Program.cs
+++ back_pp_001/Program.cs
@@ -15,6 +15,17 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 
 builder.Services.AddScoped<ICurrencyInterface, CurrencyService>();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -23,6 +34,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors("AllowAll");
 
 app.UseHttpsRedirection();
 
