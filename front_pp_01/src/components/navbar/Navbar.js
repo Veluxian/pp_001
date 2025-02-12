ed68af1eb7565198848a1fcdd742e6d2c95b9b1e
joaquin Mesa <joaquinosotelo@gmail.com>
2025-02-12 13:59:15 -0300

    Siguen las pruebas sobre la organizacion del frontend, se agrega el icono de la app

--- front_pp_01/src/components/navbar/Navbar.js
+++ front_pp_01/src/components/navbar/Navbar.js
@@ -3,12 +3,17 @@ import { Link } from "react-router-dom";
 
 export const Navbar = () => {
     return(
-        <nav class="navbar navbar-expand-lg">
-            <ul class="navbar-nav">
-                <li class="nav-item"><Link to= "/">Home</Link></li>
-                <li class="nav-item"><Link to ="/login">Login</Link></li>
-                <li class="nav-item"><Link to="/MainInventory">Main inventory</Link></li>
-            </ul>
-        </nav>
+        <div class="d-flex justify-content-center">
+            <nav class="navbar navbar-expand-lg bg-body-tertiary">
+                <div class="container-fluid">
+                    <a class="navbar-brand" href="/">MRPG</a>
+                    <ul class="navbar-nav">
+                        <li class="nav-item"><Link to= "/">Home</Link></li>
+                        <li class="nav-item"><Link to ="/login">Login</Link></li>
+                        <li class="nav-item"><Link to="/MainInventory">Main inventory</Link></li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
     )
 }
--- front_pp_01/src/pages/home/Home.js
+++ front_pp_01/src/pages/home/Home.js
@@ -3,7 +3,7 @@ import '../../assets/styles/index.scss';
 
 export const Home = () => {
     return(
-        <div className="container">
+        <div className="inicio">
             <h1>Primera prueba</h1>
             <p> Probando diferentes cosas</p>
         </div>
